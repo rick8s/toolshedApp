bb68dbf7c4a1e636d0f20290ab39f275c1b4907f
Rick8s <rick8s@hotmail.com>
2016-01-05 18:02:15 -0600

    Created Models and Tests for User and Tools

---
+++ ToolshedApp.Tests/Models/ToolTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToolshedApp.Models;
+
+namespace ToolshedApp.Tests.Models
+{
+    [TestClass]
+    public class ToolTests
+    {
+        [TestMethod]
+        public void ToolEnsureICanCreateAnInstance()
+        {
+            Tool a_tool = new Tool();
+
+            Assert.IsNotNull(a_tool);
+        }
+
+        [TestMethod]
+        public void ToolEnsureAToolHasAllOfItsInfo()
+        {
+            //Arrange
+            Tool a_tool = new Tool();
+
+            // Act 
+            a_tool.ToolId = 1;
+            a_tool.Description = "My Content";
+            a_tool.Owner = null; // Will need to define this later
+            a_tool.Image = "https://google.com";
+            a_tool.Category = "A Category";
+            a_tool.Available = true;
+
+            // Assert
+            Assert.AreEqual(1, a_tool.ToolId);
+            Assert.AreEqual("My Content", a_tool.Description);
+            Assert.AreEqual(null, a_tool.Owner);
+            Assert.AreEqual("https://google.com", a_tool.Image);
+            Assert.AreEqual("A Category", a_tool.Category);
+            Assert.AreEqual(true, a_tool.Available);
+        }
+
+
+    }
+}
---
+++ ToolshedApp/Models/Tool.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToolshedApp.Models
+{
+    public class Tool : IComparable
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public virtual ToolshedUser Owner { get; set; }
+        public string Image { get; set; }
+        [Key]
+        public int ToolId { get; set; }
+        public bool Available { get; set; }
+
+
+        public int CompareTo(object obj)
+        {
+            Tool other_tools = obj as Tool;
+            int answer = this.Name.CompareTo(other_tools.Name);
+            return answer;
+        }
+    }
+}
